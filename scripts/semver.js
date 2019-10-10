79e7795efeec5fdce28029f32990775ed45d872a
iamishraqhc <ishraq.h.c@gmail.com>
2019-10-10 17:39:33 +0600

    made some testing changes

--- scripts/semver.js
+++ scripts/semver.js
@@ -1,15 +1,19 @@
 import config from '../src/config/convict';
 import execute from './shellcmd';
+import moduleLogger from '../src/config/utils/moduleLogger';
+
+const logger = moduleLogger('semverScript');
 
 const majorFromConfig = config.get('semver.major');
 const minorFromConfig = config.get('semver.minor');
-const version = 'v0.0.0';
+const version = 'v0.0.1';
 let patch = 0;
 
-patch = `v${majorFromConfig}.${minorFromConfig}.0` === version ? patch += 1 : 0;
+patch = `v${majorFromConfig}.${minorFromConfig}.1` === version ? patch += 1 : 0;
 
 const newVersion = `v${majorFromConfig}.${minorFromConfig}.${patch}`;
 
-execute(
-    `git tag -a ${newVersion} -m "Test Tag v0.0.0"`
-  );
+// execute(`git tag -a ${newVersion} -m "Test Tag v0.0.2"`);
+execute(`git push origin master`);
+
+logger.info(`Command executed with ${newVersion}`);
