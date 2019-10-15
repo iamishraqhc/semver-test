6a227ad9db0682efcca79e0148d9cd69b777a047
ishraqhc <ishraq@craftsmenltd.com>
2019-10-15 16:53:02 +0600

    updated gitpush script test version 10

--- scripts/gitpush.js
+++ scripts/gitpush.js
@@ -1,4 +1,4 @@
-// import { execSync } from 'child_process';
+import { execSync } from 'child_process';
 import gitVersion from "git-tag-version";
 import config from '../src/config/convict';
 import moduleLogger from '../src/config/utils/moduleLogger';
@@ -7,15 +7,15 @@ const logger = moduleLogger('semverScript');
 
 const majorFromConfig = config.get('semver.major');
 const minorFromConfig = config.get('semver.minor');
-const version = 'v0.0.9';
-let patch = 8;
+const version = 'v0.0.10';
+let patch = 9;
 
-patch = `v${majorFromConfig}.${minorFromConfig}.9` === version ? patch += 1 : 0;
+patch = `v${majorFromConfig}.${minorFromConfig}.10` === version ? patch += 1 : 0;
 
 const newVersion = `v${majorFromConfig}.${minorFromConfig}.${patch}`;
 
 // eslint-disable-next-line max-len
-// execSync(`git add . && git commit -m "updated gitpush script test 4" && git push origin master`, { stdio:[0, 1, 2] });
-// execSync(`git tag -a ${newVersion} -m "Test Tag ${newVersion}" && git push --tags`, { stdio:[0, 1, 2] });
+execSync(`git add . && git commit -m "updated gitpush script test version 10" && git push origin master`, { stdio:[0, 1, 2] });
+execSync(`git tag -a ${newVersion} -m "Test Tag ${newVersion}" && git push --tags`, { stdio:[0, 1, 2] });
 logger.info(`Command executed with ${newVersion}`);
 logger.info(`The tag version from github is ${gitVersion()}`);
