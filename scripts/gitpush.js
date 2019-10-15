e959e4015e4f35f6e69375a4eb16421116a923c5
Ishraqhc <ishraq@craftsmenltd.com>
2019-10-15 16:31:56 +0600

    some changes

--- scripts/gitpush.js
+++ scripts/gitpush.js
@@ -1,4 +1,5 @@
-import { execSync } from 'child_process';
+// import { execSync } from 'child_process';
+import gitVersion from "git-tag-version";
 import config from '../src/config/convict';
 import moduleLogger from '../src/config/utils/moduleLogger';
 
@@ -14,6 +15,7 @@ patch = `v${majorFromConfig}.${minorFromConfig}.9` === version ? patch += 1 : 0;
 const newVersion = `v${majorFromConfig}.${minorFromConfig}.${patch}`;
 
 // eslint-disable-next-line max-len
-execSync(`git add . && git commit -m "updated gitpush script test 4" && git push origin master`, { stdio:[0, 1, 2] });
-execSync(`git tag -a ${newVersion} -m "Test Tag ${newVersion}" && git push --tags`, { stdio:[0, 1, 2] });
+// execSync(`git add . && git commit -m "updated gitpush script test 4" && git push origin master`, { stdio:[0, 1, 2] });
+// execSync(`git tag -a ${newVersion} -m "Test Tag ${newVersion}" && git push --tags`, { stdio:[0, 1, 2] });
 logger.info(`Command executed with ${newVersion}`);
+logger.info(`The tag version from github is ${gitVersion()}`);
