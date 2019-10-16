44e8e1224da5ed76c2b179e3c50e5973526c7513
ishraqhc <ishraq@craftsmenltd.com>
2019-10-16 17:38:23 +0600

    updated gitpush script test version v0.0.0

--- scripts/gitpush.js
+++ scripts/gitpush.js
@@ -9,7 +9,7 @@ const logger = moduleLogger('semverScript');
 const majorFromConfig = config.get('semver.major');
 const minorFromConfig = config.get('semver.minor');
 const version = `v${packageJson.version}`;
-let patch = 12;
+let patch = 13;
 
 patch = `v${majorFromConfig}.${minorFromConfig}.${patch}` === version ? patch += 1 : 0;
 
@@ -18,6 +18,6 @@ const newVersion = `${majorFromConfig}.${minorFromConfig}.${patch}`;
 // eslint-disable-next-line max-len
 execSync(`git add . && git commit -m "updated gitpush script test version v${newVersion}" && git push origin master`, { stdio:[0, 1, 2] });
 execSync(`git tag -a v${newVersion} -m "Test Tag v${newVersion}" && git push --tags`, { stdio:[0, 1, 2] });
-// execSync(`npm version ${newVersion}`, { stdio:[0,1,2] });
+execSync(`npm publish`, { stdio:[0,1,2] });
 logger.info(`Command executed with v${newVersion}`);
 logger.info(`The tag version from github is ${gitVersion()}`);
