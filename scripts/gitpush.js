b7c8f31b75b0874e9c304d0250c16525e05e62dc
ishraqhc <ishraq@craftsmenltd.com>
2019-10-16 17:41:00 +0600

    updated gitpush script test version v0.0.0

--- scripts/gitpush.js
+++ scripts/gitpush.js
@@ -17,7 +17,9 @@ const newVersion = `${majorFromConfig}.${minorFromConfig}.${patch}`;
 
 // eslint-disable-next-line max-len
 execSync(`git add . && git commit -m "updated gitpush script test version v${newVersion}" && git push origin master`, { stdio:[0, 1, 2] });
-execSync(`git tag -a v${newVersion} -m "Test Tag v${newVersion}" && git push --tags`, { stdio:[0, 1, 2] });
-execSync(`npm publish`, { stdio:[0,1,2] });
+// execSync(`git tag -a v${newVersion} -m "Test Tag v${newVersion}" && git push --tags`, { stdio:[0, 1, 2] });
+
+// eslint-disable-next-line max-len
+execSync(`npm version ${newVersion} && git tag -a v${newVersion} -m "Test Tag v${newVersion}" && git push --tags`, { stdio:[0,1,2] });
 logger.info(`Command executed with v${newVersion}`);
 logger.info(`The tag version from github is ${gitVersion()}`);
